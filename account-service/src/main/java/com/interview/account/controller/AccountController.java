8470f9362b34443f050db12c7b36fb94e7e9f215
Mahdad <mahdad_12@yahoo.com>
2023-03-26 19:02:47 +0300

    update github action in order to build and publish a docker image into docker private repository for frontend project

--- account-service/src/main/java/com/interview/account/controller/AccountController.java
+++ account-service/src/main/java/com/interview/account/controller/AccountController.java
@@ -14,6 +14,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.NonNull;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
